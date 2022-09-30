74c5f8e80133c738bd122b7edcfdb8bb3135ede1
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-09-29 21:42:59 -0500

    adds some pseudo code to js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+var hour = document.getElementById('hour-num')
+var textArea = document.querySelector('textarea')
+
+var currentTime = moment("hh");
+
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"))
 
-
+var blockDisplay = function() {
+    if(hour > currentTime) {
+        textArea.style = '.future';
+    } else if(hour < currentTime) {
+        textArea.style = '.past';
+}
+}
+//if hour is < current time
+    //style= past
+//else if hour is > current time
+    //style = future
+//else style = present
