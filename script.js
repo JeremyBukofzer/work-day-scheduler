5b6263e6a74f80014551b755e6aed6020c7010eb
Jeremy Bukofzer <jlbukofzer@gmail.com>
2022-10-02 19:12:06 -0500

    adds parse to timeblock js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 
-
+// Displays current date at top of page
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"))
 
+// Selects current time using moment.js
+var currentTime = moment().hour();
+
+
 function blockDisplay() {
-    var currentTime = moment().format('h A');
     
     $('.time-block').each(function() {
-        
-    var blockHour = $('.hour').text()
+        var timeBlock = parseInt($(this).attr('id'))
 
-            if (blockHour > currentTime) {
-                $('.time-block').addClass = '.future';
-            } else if (blockHour < currentTime) {
-                $('.time-block').addClass = '.past';
-            } else {
-                $('.time-block').addClass = '.present';
+            if (timeBlock > currentTime) {
+                $(this).addClass('past')
+            } else if (timeBlock < currentTime) {
+                $(this).addClass('future')
+            } else if (timeBlock === currentTime) {
+                $(this).addClass('present')
             }
             console.log(blockHour)
             console.log(currentTime)
         })
     }
+
+    
 
 blockDisplay()
